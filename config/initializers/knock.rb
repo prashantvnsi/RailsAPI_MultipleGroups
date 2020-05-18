91d6133fa190a5480845ee1fd45640ccfebf52b5
prashant.tiwari <prashant.tiwari@blacklane.com>
2020-05-18 11:41:32 +0200

    Updating secret key file

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -37,10 +37,7 @@ Knock.setup do |config|
   ##
   ## Default:
   #config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
-
-  # For Auth0
-  config.token_audience = -> { Rails.application.secrets.auth0_client_id }
-  config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
+  config.token_secret_signature_key = -> { Rails.application.credentials.read }
 
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
